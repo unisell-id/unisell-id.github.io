eec365380f8deec0badd92cb5846fa2b50078dae
Syaiful Nur Wardani <echo.khannedy@gmail.com>
2022-03-15 13:46:43 +0700

    rev

--- src/components/Accordion.vue
+++ src/components/Accordion.vue
@@ -1,24 +1,26 @@
 <template>
   <!-- blum interaktif? on progres? -->
-  <div>
-    <div class="">
+  <div class="rounded-md">
+    <div>
       <div class="bg-red-900 text-white p-4 font-bold flex justify-start">
         <ArrowDown />
         <p class="px-2">Apa yang dapat Unisell berikan kepada reseller ?</p>
       </div>
       <div class="bg-white">
         <slot></slot>
+        <hr />
       </div>
       <div class="bg-red-900 text-white p-4 font-bold flex justify-start">
         <ArrowRight class="w-6" />
         <p class="px-2">Apakah ada platform fee untuk menggunakan Unisell ?</p>
       </div>
+      <hr class="text-white" />
 
       <div class="bg-red-900 text-white p-4 font-bold flex justify-start">
         <ArrowRight class="w-6" />
         <p class="px-2">FAQ</p>
       </div>
-
+      <hr class="text-white" />
       <div class="bg-red-900 text-white p-4 font-bold flex justify-start">
         <ArrowRight class="w-6" />
         <p class="px-2">FAQ</p>
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -35,15 +35,16 @@
 
   <section class="bg-orange-100 py-32 px-16">
     <div
-      class="flex flex-col lg:flex-row w-full max-w-screen-lg mx-auto gap-x-20 gap-y-10" >
+      class="flex flex-col lg:flex-row w-full max-w-screen-lg mx-auto gap-x-20 gap-y-10"
+    >
       <h4 class="font-semibold text-2xl text-stone-700">
         Apakah hal-hal seperti ini menjadi keresahan anda?
       </h4>
 
       <ul class="list-disc text-lg font-semibold text-orange-500 space-y-3">
-        <li>Mau bisnis tapi jualan apa ya? 	&#129300</li>
-        <li>Aku udah punya ide jualan, tapi nggak punya modal 	&#128577</li>
-        <li >Kalau jualan aku nggak punya tempat buat menaruh daganganku 😫</li>
+        <li>Mau bisnis tapi jualan apa ya? 🤔</li>
+        <li>Aku udah punya ide jualan, tapi nggak punya modal ☹</li>
+        <li>Kalau jualan aku nggak punya tempat buat menaruh daganganku 😫</li>
         <li>Eh cara jualan yang benar bagaimana ya? 😅</li>
         <li>
           Menjalani bisnis sendiri, seandainya aku punya tempat buat
@@ -56,12 +57,13 @@
   <!-- samakan diatas -->
   <section class="bg-amber-500 py-32 px-16">
     <div
-      class="flex flex-col lg:flex-row w-full max-w-screen-lg mx-auto gap-x-20 gap-y-10">
-       <h4 class="font-semibold text-2xl text-stone-700">
+      class="flex flex-col lg:flex-row w-full max-w-screen-lg mx-auto gap-x-20 gap-y-10"
+    >
+      <h4 class="font-semibold text-2xl text-stone-700">
         Atau.. mungkin keresahan anda seperti ini?
       </h4>
       <ul class="list-disc text-lg font-semibold text-white space-y-3">
-        <li>Susah juga ya bikin produk sama jualan. &#128556</li>
+        <li>Susah juga ya bikin produk sama jualan. 😬</li>
         <li>
           Wah aku suka bikin kerajinan, tapi aku gak bisa <br />jualan 😅
           (*merasa tidak menghasilkan).
@@ -69,76 +71,61 @@
         <li>Bisa tidak ya usahaku cuma fokus produksi aja?</li>
         <li>
           Mau meningkatkan produksi tapi kalau tidak terjual bagaimana ya? Niat
-          cari untung lebih malah buntung.  &#128578
+          cari untung lebih malah buntung. 🙂
         </li>
       </ul>
-
-
     </div>
   </section>
 
-
-  <section class="bg-orange-100 p-4  lg:py-32 lg:px-16  ">
+  <section class="bg-orange-100 p-4 lg:py-32 lg:px-16">
     <!-- wrapper flex buat max-width -->
-      <div class=" flex flex-col sm:flex-row w-full p-4 justify-center gap-x-4 gap-y-1 ">
-        <img src="/img/BrandLogo.png " />
-       </div>
+    <div
+      class="flex flex-col sm:flex-row w-full p-4 justify-center gap-x-4 gap-y-1"
+    >
+      <img src="/img/BrandLogo.png " />
+    </div>
 
-    <div class="flex flex-col lg:flex-row w-full max-w-6xl mx-auto gap-x-4 gap-y-1 items-center ">
-
-
-      <div class=" py-10 w-3/4">
-        <h4 class="text-xl  text-stone-700">
-        <b>Sekilas</b>
+    <div
+      class="flex flex-col lg:flex-row w-full max-w-6xl mx-auto gap-x-4 gap-y-1 items-center"
+    >
+      <div class="py-10 w-3/4">
+        <h4 class="text-xl text-stone-700">
+          <b>Sekilas</b>
         </h4>
-        <br>
-        <div class=" text-stone-500 font-medium text-base w-3/5">
-
-           Untuk anda yang ingin memulai bisnis maupun yang ingin
-          mengembangkan bisnisnya (reseller dan supplier), Marketplace B2B
-          yang menyediakan cara kerja yang simpel dan mudah sehingga
-          meminimalisir usaha anda dalam menjalankan bisnis serta
-          memasarkan produk anda.
-
-
-
-      </div>
-
-      </div>
-
-
-        <div class="py-10 w-1/2  ">
-          <h4 class="text-xl ">
-            <b class="text-stone-700">Apa yang bukan kita?</b>
-          </h4>
-          <br>
-          <div class="text-stone-500 text-base font-medium ">
-
-              Kami bukan marketplace untuk berbelanja layaknya e-commerce pada umumnya. Sistem kita hanya menerima reseller dan
-              supplier.
-          </div>
-            <div class="py-4  flex justify-between">
-           <router-link
-          to="#!"
-          class="flex px-4 py-2 bg-amber-500 hover:bg-orange-500 text-white justify-center"
-        >
-          <span class="font-medium text-base">Pelajari Kita Lebih Lengkap</span>
-          <ArrowRight class="w-6" />
-        </router-link>
+        <br />
+        <div class="text-stone-500 font-medium text-base w-3/5">
+          Untuk anda yang ingin memulai bisnis maupun yang ingin mengembangkan
+          bisnisnya (reseller dan supplier), Marketplace B2B yang menyediakan
+          cara kerja yang simpel dan mudah sehingga meminimalisir usaha anda
+          dalam menjalankan bisnis serta memasarkan produk anda.
         </div>
-
-        </div>
-
       </div>
 
-
-
-
-
-
+      <div class="py-10 w-1/2">
+        <h4 class="text-xl">
+          <b class="text-stone-700">Apa yang bukan kita?</b>
+        </h4>
+        <br />
+        <div class="text-stone-500 text-base font-medium">
+          Kami bukan marketplace untuk berbelanja layaknya e-commerce pada
+          umumnya. Sistem kita hanya menerima reseller dan supplier.
+        </div>
+        <div class="py-4 flex justify-between">
+          <router-link
+            to="#!"
+            class="flex px-4 py-2 bg-amber-500 hover:bg-orange-500 text-white justify-center"
+          >
+            <span class="font-medium text-base"
+              >Pelajari Kita Lebih Lengkap</span
+            >
+            <ArrowRight class="w-6" />
+          </router-link>
+        </div>
+      </div>
+    </div>
   </section>
 
-  <section class="bg-amber-500 text-2xl ">
+  <section class="bg-amber-500 text-2xl">
     <h4 class="mx-20 py-8 text-white">
       <b>Bagaimana Cara Kita Bekerja</b>
     </h4>
@@ -155,31 +142,22 @@
           <span class="text-gradient">FAQ</span>
         </h4>
       </div>
-
-
-      <div class="rounded">
+      <div class="flex">
         <Accordion>
-
-
-        <ul class=" py-6 px-6 list-disc space-y-3 text-stone-500"> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod
-          tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim
-          veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
-          ea
-          commodo consequat.
-        </ul>
-
-
-      </Accordion>
+          <ul class="py-6 px-6 list-disc space-y-3 text-stone-500">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat.
+          </ul>
+        </Accordion>
       </div>
-
     </div>
   </section>
 
   <!--  -->
-  <footer class="bg-stone-900 p-8 lg:py-12 lg:px-10 ">
-    <div class="flex flex-row space-x-32 justify-between md:max-w-full  ">
+  <footer class="bg-stone-900 p-8 lg:py-12 lg:px-10">
+    <div class="flex flex-row space-x-32 justify-between md:max-w-full">
       <div>
         <img src="Unisell-Brand_Typo.svg" alt="logo" class="w-1/2" />
         <p class="text-stone-300">
@@ -187,11 +165,17 @@
           All right reserved.
         </p>
       </div>
-      <div class="flex flex-col space-y-2 text-white max-w-lg" >
+      <div class="flex flex-col space-y-2 text-white max-w-lg">
         <h5 class="text-xs text-stone-300">Navigasi</h5>
-        <router-link to="/" class="block w-full hover:underline">Beranda</router-link><br />
-        <router-link to="/tentang" class="bock w-full hover:underline">Tentang</router-link><br />
-        <router-link to="/" class="block w-full hover:underline">Katalog</router-link><br />
+        <a href="#" class="block w-full hover:underline">Beranda</a>
+
+        <br />
+        <router-link to="/tentang" class="bock w-full hover:underline"
+          >Tentang</router-link
+        ><br />
+        <router-link to="/" class="block w-full hover:underline"
+          >Katalog</router-link
+        ><br />
       </div>
       <div class="flex flex-col space-y-2 text-white">
         <h5 class="text-xs text-stone-300">Media Sosial</h5>
@@ -217,7 +201,6 @@ import Navbar from "../components/Navbar.vue";
 import Accordion from "../components/Accordion.vue";
 import ArrowRight from "../components/icons/ArrowRight.vue";
 import Instagram from "../components/icons/Instagram.vue";
-
 </script>
 
 <style lang="sass" scoped>
